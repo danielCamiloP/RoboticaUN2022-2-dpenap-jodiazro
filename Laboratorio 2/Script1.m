d261bee9c4a621c1bd044a9483aebf26927cfa9b
dpenap <dpenap@unal.edu.co>
2022-09-15 22:26:49 -0500

    Agregada funcionalidad de movimiento relativo y absoluto

--- Laboratorio 2/Script1.m
+++ Laboratorio 2/Script1.m
@@ -1,3 +1,7 @@
+%% Reinicio de variables y consola
+clc;
+clear;
+
 %% inicialización de ROS
 rosinit; %conexion con nodo maestro
 
@@ -12,7 +16,7 @@ pause(1)
 
 %% Sucripción al tópico de pose
 poseSub = rossubscriber("/turtle1/pose",'turtlesim/Pose'); %Creación del suscriptor
-poseMsg = poseSub.LatestMessage; %Obtención del último mensaje del tópico PoseS
+poseMsg = poseSub.LatestMessage %Obtención del último mensaje del tópico PoseS
 %% Envío de valores asociados a la pose de turtle1
 %Para poder modificar la pose de la tortuga de manera directa, se deben
 %emplear servicios, como lo indica la wiki de ros http://wiki.ros.org/turtlesim
@@ -20,11 +24,36 @@ poseMsg = poseSub.LatestMessage; %Obtención del último mensaje del tópico Pos
 %teleport_relative. Como se indica en el nombre, uno permite emplear
 %coordenadas absolutas mientras el otro permite usar coordenadas relativas.
 
-%ejemplo con coordenadas absolutas
+%%ejemplo con coordenadas absolutas
+%creación del cliente
+clAbs = rossvcclient('/turtle1/teleport_absolute');
+msgAbs = rosmessage(clAbs);
 
-%ejemplo con coordenadas relativas
+% Asignación de valores
+msgAbs.X = 5; %mover la tortuga a x=5
+msgAbs.Y = 8; %mover la tortuga a y=8
+msgAbs.Theta = (3/5) *pi; %hacer que la tortuga rote hasta 3pi/5
 
+%envio al cliente
+responseAbs = call(clAbs,msgAbs);
+pause(1);
 
+% Como se puede ver, la sintaxis es muy similar a emplear un publisher.
+
+%% ejemplo con coordenadas relativas
+clRel = rossvcclient('/turtle1/teleport_relative');
+msgRel = rosmessage(clRel);
+
+% Asignación de valores
+msgRel.Linear = 3; %avanzar 3 unidades hacia donde apunta la tortuga
+msgRel.Angular = pi/2; %rotar la tortuga pi/2 rad
+
+%envio al cliente
+responseRel = call(clRel,msgRel);
+pause(1);
+
+%Si se corre 4 veces este código la tortuga realizará un cuadrado desde su
+%posición en el momento de ejecutar la sección por primera vez
 %% Finalización del nodo maestro de ROS
 %Consultando la documentación de ROSToolbox, se debe ejecutar
 rosshutdown;

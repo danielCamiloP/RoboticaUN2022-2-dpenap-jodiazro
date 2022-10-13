289d18c69dc047fe668bb29a6ce6472817cd7d23
Daniel <dpenap@unal.edu.co>
2022-10-13 17:45:21 -0500

    Agregada repeticion de ciclos y tiempos de espera

--- Laboratorio 4/dynamixel_one_motor/scripts/homeServMovement.py
+++ Laboratorio 4/dynamixel_one_motor/scripts/homeServMovement.py
@@ -41,18 +41,31 @@ jointCommand('', 3, 'Torque_Limit', 400, 0)
 jointCommand('', 4, 'Torque_Limit', 400, 0)
 jointCommand('', 5, 'Torque_Limit', 400, 0)
 
-# Home Position
+cycleNumber = 2;
 
-jointCommand('', 1, 'Goal_position', 0, 0.5)
-jointCommand('', 2, 'Goal_position', 0, 0.5)
-jointCommand('', 3, 'Goal_position', 0, 0.5)
-jointCommand('', 4, 'Goal_position', 0, 0.5)
-jointCommand('', 5, 'Goal_position', 0, 0.5)
+while(cycleNumber>0):
+    # Home Position
+    jointCommand('', 1, 'Goal_position', 0, 0.5)
+    time.sleep(0.5)
+    jointCommand('', 2, 'Goal_position', 0, 0.5)
+    time.sleep(0.5)
+    jointCommand('', 3, 'Goal_position', 0, 0.5)
+    time.sleep(0.5)
+    jointCommand('', 4, 'Goal_position', 0, 0.5)
+    time.sleep(0.5)
+    jointCommand('', 5, 'Goal_position', 0, 0.5)
+    time.sleep(2)
 
-# Goal Position [45 30 10 -15 0]
+    # Goal Position [45 30 10 -15 0]
+    jointCommand('', 1, 'Goal_position', pi/4, 0.5)
+    time.sleep(0.5)
+    jointCommand('', 2, 'Goal_position', pi/6, 0.5)
+    time.sleep(0.5)
+    jointCommand('', 3, 'Goal_position', pi/12, 0.5)
+    time.sleep(0.5)
+    jointCommand('', 4, 'Goal_position', -pi/12, 0.5)
+    time.sleep(0.5)
+    jointCommand('', 5, 'Goal_position', 0, 0.5)
+    time.sleep(2)
 
-jointCommand('', 1, 'Goal_position', pi/4, 0.5)
-jointCommand('', 2, 'Goal_position', pi/6, 0.5)
-jointCommand('', 3, 'Goal_position', pi/12, 0.5)
-jointCommand('', 4, 'Goal_position', -pi/12, 0.5)
-jointCommand('', 5, 'Goal_position', 0, 0.5)
+    cycleNumber = cycleNumber - 1

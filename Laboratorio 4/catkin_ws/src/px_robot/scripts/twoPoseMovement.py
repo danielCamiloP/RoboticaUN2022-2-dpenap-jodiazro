2bcd1f2b9ee8b135f32ef9207688d5fb70475f71
dpenap <dpenap@unal.edu.co>
2022-10-18 11:30:22 -0500

    Update al ws de catkin. Update a px_robot de dynamixel_one_motor. Incluido codigos para mov 2pose

---
+++ Laboratorio 4/catkin_ws/src/px_robot/scripts/twoPoseMovement.py
@@ -0,0 +1,53 @@
+from cmath import pi
+import rospy
+import numpy as np
+import math
+from std_msgs.msg import String
+from sensor_msgs.msg import JointState
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+
+
+def joint_publisher():
+    pub = rospy.Publisher('/joint_trajectory', JointTrajectory, queue_size=0)
+    rospy.init_node('joint_publisher', anonymous=False)
+    
+    while not rospy.is_shutdown():
+        key=input()
+
+        state = JointTrajectory()
+        state.header.stamp = rospy.Time.now()
+        state.joint_names = ["joint_1", "joint_2", "joint_3", "joint_4", "joint_5"]
+        point = JointTrajectoryPoint()
+
+        if key == 'f':
+            aux=[-45*pi/180,45*pi/180,45*pi/180,45*pi/180,0]
+            key=' '
+        elif key == 'h':
+            aux= point.positions = [0,0,-pi/2,0,0]
+            key=' '
+
+        point.positions = aux
+        point.time_from_start = rospy.Duration(0.5)
+        state.points.append(point)
+        pub.publish(state)
+        print('published command')
+        rospy.sleep(1)
+
+        state = JointTrajectory()
+        state.header.stamp = rospy.Time.now()
+        state.joint_names = ["joint_1", "joint_2", "joint_3", "joint_4", "joint_5"]
+        point = JointTrajectoryPoint()
+        point.positions = [0.25, 0, 0, 0, 1.3]    
+        point.time_from_start = rospy.Duration(0.5)
+        state.points.append(point)
+        pub.publish(state)
+        print('published command')
+        rospy.sleep(1)
+
+if __name__ == '__main__':
+    try:
+        joint_publisher()
+    except rospy.ROSInterruptException:
+        pass
+
+

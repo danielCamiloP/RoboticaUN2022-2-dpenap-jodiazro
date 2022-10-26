f0c26f56a34964a33a7a447777bf22f22a6ff8bd
NEWjadr2000 <johandiazr@hotmail.com>
2022-10-26 08:03:37 -0500

    cinematica inversa

---
+++ Laboratorio 5/lab5.m
@@ -0,0 +1,21 @@
+L = [46 107 107 110];
+DHParams = [0 L(1) 0 pi/2 0 0;
+            0 0 L(2) 0 0 pi/2;
+            0 0 L(3) 0 0 0;
+            0 0 L(4) 0 0 0];            
+
+Lnk(1) = Link(DHParams(1,:));
+Lnk(2) = Link(DHParams(2,:));
+Lnk(3) = Link(DHParams(3,:));
+Lnk(4) = Link(DHParams(4,:));
+
+
+R = SerialLink(Lnk,'name','PhantomX');
+
+H_tool = [0 0 1 0;
+          1 0 0 0;
+          0 1 0 0;
+          0 0 0 1];
+
+R.tool = H_tool;
+
